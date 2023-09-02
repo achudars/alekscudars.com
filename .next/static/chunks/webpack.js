1a0637ed3709adcfb3ee7b7fe12bf64f8a1a481b
aleks <achudars@gmail.com>
2023-09-02 22:29:49 +0100

    feat: update node-sass from v7 to v9

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -153,7 +153,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "247f4ff8e1609e79"; }
+/******/ 		__webpack_require__.h = function() { return "58b2b8f4da934b61"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
