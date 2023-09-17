a8c4ffd0a7c8d78fd3985ba5af95b63955836044
aleks <achudars@gmail.com>
2023-09-17 20:17:55 +0100

    feat: add Do Nothing to read books

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -153,7 +153,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "e8411f0917adf55a"; }
+/******/ 		__webpack_require__.h = function() { return "8aa7988603c5f099"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
