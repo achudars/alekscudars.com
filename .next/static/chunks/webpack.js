ce5642b6c91e557955b861892eec72d7d0326e8e
aleks <achudars@gmail.com>
2023-09-17 20:15:51 +0100

    feat: add A Mind for Numbers to read books

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -153,7 +153,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "326fd9cb06e5b7fb"; }
+/******/ 		__webpack_require__.h = function() { return "e8411f0917adf55a"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- .next/static/webpack/webpack.739a439ae926cd5a.hot-update.js
+++ .next/static/webpack/webpack.739a439ae926cd5a.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "9869f0066111323c"; }
+/******/ 	__webpack_require__.h = function() { return "e8411f0917adf55a"; }
 /******/ }();
 /******/ 
 /******/ }
