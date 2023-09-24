3c856ecd1230ce9ac286d4c41d19c3e803ece0f1
aleks <achudars@gmail.com>
2023-09-24 19:22:30 +0100

    feat: add Drift into Failure to dropped books

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -153,7 +153,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "8aa7988603c5f099"; }
+/******/ 		__webpack_require__.h = function() { return "30dee09b607746ee"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
