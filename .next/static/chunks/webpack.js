5ff769129d2c324bb52f960cfd3d39457daa353d
aleks <achudars@gmail.com>
2023-10-09 06:56:14 +0100

    chore: update minor deps

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -153,7 +153,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "5f5c2c6f2be00106"; }
+/******/ 		__webpack_require__.h = function() { return "7980d7ff98c3da6e"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
