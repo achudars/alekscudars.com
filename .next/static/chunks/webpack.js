ec7ff6afdb45b61393f7fa5449959ec61a307240
aleks <achudars@gmail.com>
2023-10-15 21:07:42 +0100

    feat: add a summary of the Origina of species

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -153,7 +153,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "bb3bb52039cc5367"; }
+/******/ 		__webpack_require__.h = function() { return "7541dae304548963"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
