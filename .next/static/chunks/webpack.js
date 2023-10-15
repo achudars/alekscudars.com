706b3238b20e8e41f1c1ce365eec7346916521ce
aleks <achudars@gmail.com>
2023-10-15 21:30:39 +0100

    feat: add summary of Drift into Failure

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -153,7 +153,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "4cee78d421988847"; }
+/******/ 		__webpack_require__.h = function() { return "40418a2ec8a520ee"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
