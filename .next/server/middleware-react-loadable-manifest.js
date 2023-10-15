721b6dc1b6f0a3a4e7e40558c92d1b19d25b09b1
aleks <achudars@gmail.com>
2023-10-15 20:06:33 +0100

    feat: update what is in progress

--- .next/server/middleware-react-loadable-manifest.js
+++ .next/server/middleware-react-loadable-manifest.js
@@ -1,1 +1,1 @@
-self.__REACT_LOADABLE_MANIFEST="{\"index.js -> ../src/components/Portfolio\":{\"id\":\"index.js -> ../src/components/Portfolio\",\"files\":[\"static/chunks/src_components_Portfolio_js.js\"]}}"
+self.__REACT_LOADABLE_MANIFEST="{}"
