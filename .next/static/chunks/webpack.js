2635bc7e247a7e2f20b864e8cdea0985249e6aec
aleks <achudars@gmail.com>
2023-10-17 19:34:17 +0100

    feat: add summary for Invisible Women

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -153,7 +153,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "3b8f80a355d4ef02"; }
+/******/ 		__webpack_require__.h = function() { return "0d894f180d3567e9"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
