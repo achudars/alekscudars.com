246694d2ab2ba4420e47454e6ac60c08fe79490e
aleks <achudars@gmail.com>
2023-10-19 16:34:07 +0100

    feat: add summary for Predictably Irrational

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -153,7 +153,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "5df3546033495cfd"; }
+/******/ 		__webpack_require__.h = function() { return "f102056f49b74a87"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
