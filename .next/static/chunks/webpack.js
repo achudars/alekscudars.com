933f57309155b7f6bc43c37d85f23e14ccc13bb8
aleks <achudars@gmail.com>
2023-10-24 21:08:01 +0100

    feat: add summary for The Body

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -153,7 +153,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "be9a7884c7143599"; }
+/******/ 		__webpack_require__.h = function() { return "cbdd239aca58b303"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
