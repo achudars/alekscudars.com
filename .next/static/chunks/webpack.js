099e4a698207e5401694f8b8444b70b08442c970
aleks <achudars@gmail.com>
2023-10-25 21:53:06 +0100

    feat: add summary for The Undoing Project

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -153,7 +153,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "ebed78abf75766a0"; }
+/******/ 		__webpack_require__.h = function() { return "49cace846fff347b"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
