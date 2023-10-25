1208ccfe7e38c5366d9c17d7dd03c46ad9f39a85
aleks <achudars@gmail.com>
2023-10-25 21:58:47 +0100

    feat: add summary of Make Your Bed

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -153,7 +153,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "5d7eb05c06c3b846"; }
+/******/ 		__webpack_require__.h = function() { return "62f76d3380a8a305"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
