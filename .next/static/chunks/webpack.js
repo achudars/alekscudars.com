2112c7b604156a220a6d1525c4e6fdc10b5159ad
aleks <achudars@gmail.com>
2023-10-25 21:57:14 +0100

    feat: add summary for The Psychology of Security

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -153,7 +153,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "cd5e16b3414c7b53"; }
+/******/ 		__webpack_require__.h = function() { return "5d7eb05c06c3b846"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
