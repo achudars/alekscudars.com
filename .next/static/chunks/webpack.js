c27354b1f1ea6948615b24c40f9f2248dde1d1c1
aleks <achudars@gmail.com>
2023-10-27 22:31:56 +0100

    feat: add summary of Never Eat Alone

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -153,7 +153,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "3a2bb4c1eec2bdfc"; }
+/******/ 		__webpack_require__.h = function() { return "5fecde2a139a41c8"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
