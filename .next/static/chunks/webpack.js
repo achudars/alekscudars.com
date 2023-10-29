379e580ae60b664fb7a484144ada7ca2744f4a42
aleks <achudars@gmail.com>
2023-10-29 11:58:43 +0000

    feat: add summary for Software Architecture Patterns

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -153,7 +153,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "497b37520808ed19"; }
+/******/ 		__webpack_require__.h = function() { return "ee78bcd1ad3bb447"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
