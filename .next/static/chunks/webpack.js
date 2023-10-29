bfc4f7b7a7e5ff6e98d588d306b8b9bb406d8f79
aleks <achudars@gmail.com>
2023-10-29 11:57:33 +0000

    feat: add summary for The Illusion of Choice

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -153,7 +153,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "3aec0db7f69d00b7"; }
+/******/ 		__webpack_require__.h = function() { return "497b37520808ed19"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- .next/static/webpack/webpack.7d8b33226ddd98a8.hot-update.js
+++ .next/static/webpack/webpack.7d8b33226ddd98a8.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "848225b1dc0d4e73"; }
+/******/ 	__webpack_require__.h = function() { return "9cc7fe22c05b751f"; }
 /******/ }();
 /******/ 
 /******/ }
