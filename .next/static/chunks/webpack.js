d52b866b42af8f1b603b15e5aef2c4cdac7ebcb3
aleks <achudars@gmail.com>
2023-10-29 12:37:30 +0000

    feat: add summary for Sapiens

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -153,7 +153,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "333bcbbe10ae93c3"; }
+/******/ 		__webpack_require__.h = function() { return "bafaec5c53454cc1"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
