f6c1f0e0df6af0f79abbc020e4212767f1f022c4
aleks <achudars@gmail.com>
2023-10-29 12:15:06 +0000

    feat: add summary of Potato

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -153,7 +153,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "e23270de7d73ccec"; }
+/******/ 		__webpack_require__.h = function() { return "74a07d821f32fb0b"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
