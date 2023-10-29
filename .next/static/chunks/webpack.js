9ec98a37332f6cc8216be959657b2489c1d50b6d
aleks <achudars@gmail.com>
2023-10-29 12:12:49 +0000

    feat: add summary of The Moment of Lift

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -153,7 +153,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "9cd5e83b397f81d8"; }
+/******/ 		__webpack_require__.h = function() { return "e23270de7d73ccec"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
