3f4dae661e49a3d8fdd5e63e82df42faf8038d73
aleks <achudars@gmail.com>
2023-12-09 19:42:52 +0000

    feat: add running stats

--- pages/running-and-training.js
+++ pages/running-and-training.js
@@ -1,6 +1,47 @@
+/* eslint-disable @next/next/no-img-element */
 import Layout from "../src/layout/Layout";
 
 const RunningAndTraining = () => {
+  const renderOverviewStats = () => {
+    return (
+      <div className="col-sm-6 col-md-6 col-xl-12">
+        <div className="rb-left">
+          <img
+            src={`/static/img/running/nike-overview.png`}
+            title={"overview by Nike"}
+            alt={"overview by Nike"}
+          />
+        </div>
+      </div>
+    );
+  };
+
+  const renderAllTimeStats = () => {
+    return (
+      <div className="col-sm-6 col-md-6 col-xl-12">
+        <div className="rb-right">
+          <img
+            src={`/static/img/running/nike-all-time-stats.png`}
+            title={"all-time stats by Nike"}
+            alt={"all-time stats by Nike"}
+          />
+        </div>
+      </div>
+    );
+  };
+
+  const renderExtraDetailSection = () => {
+    return (
+      <div className="resume-box">
+        <div className="resume-row">
+          <div className="row">
+            {renderOverviewStats()}
+            {renderAllTimeStats()}
+          </div>
+        </div>
+      </div>
+    );
+  };
 
   const renderContainer = () => {
     return (
@@ -8,6 +49,7 @@ const RunningAndTraining = () => {
         <div className="title">
           <h3>Running and Training.</h3>
         </div>
+        {renderExtraDetailSection()}
       </>
     );
   };
