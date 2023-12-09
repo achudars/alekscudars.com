6c3c1cc6a5734c409e51afcaa6bdac1c6fdb43cd
aleks <achudars@gmail.com>
2023-12-09 16:35:37 +0000

    chore: only keep podcasts I still listen to regularly

---
+++ .next/static/webpack/pages/podcasts.b372c78588e3a0b5.hot-update.js
@@ -0,0 +1,22 @@
+"use strict";
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+self["webpackHotUpdate_N_E"]("pages/podcasts",{
+
+/***/ "./pages/api/podcasts.json":
+/*!*********************************!*\
+  !*** ./pages/api/podcasts.json ***!
+  \*********************************/
+/***/ (function(module, __unused_webpack_exports, __webpack_require__) {
+
+module.exports = JSON.parse('[{"podcastTitle":"The Infinite Monkey Cage","podcaster":"Brian Cox and Robin Ince","podcastLink":"https://pod.link/343580439"},{"podcastTitle":"КритМышь","podcaster":"Александр Головин","podcastLink":"https://pod.link/1385753390"},{"podcastTitle":"Einfach Deutsch","podcaster":"Anika Würz","podcastLink":"https://pod.link/1630788864"},{"podcastTitle":"Podkāsts MUŠA","podcaster":"Agate Mežule, Džemma Sudraba, Anete Valaine, Anna Katrīna Elme, Linards Romka un Marta Herca","podcastLink":"https://pod.link/1533694069"}]');
+
+/***/ })
+
+});
