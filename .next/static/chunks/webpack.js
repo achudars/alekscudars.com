d8bfea3cfab1af0a0419e096f8c856b094317e9a
aleks <achudars@gmail.com>
2024-01-10 21:43:34 +0000

    feat: start Boomerang

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -153,7 +153,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "1b6640c157673ea4"; }
+/******/ 		__webpack_require__.h = function() { return "b97e5861457a8c4d"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- .next/static/webpack/webpack.caaafdf992249188.hot-update.js
+++ .next/static/webpack/webpack.caaafdf992249188.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "7eb642216973e0f3"; }
+/******/ 	__webpack_require__.h = function() { return "b97e5861457a8c4d"; }
 /******/ }();
 /******/ 
 /******/ }
