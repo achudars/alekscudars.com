21ce20d1321e3a4b66db66787c9506931625e9a3
aleks <achudars@gmail.com>
2024-02-04 10:10:48 +0000

    feat: add summary for boomerang

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -153,7 +153,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "ec3262f186260ddc"; }
+/******/ 		__webpack_require__.h = function() { return "b1faf0486074d4a5"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
