e00c1edc652536e06dba9b73c81d79112a6b9f9e
aleks <achudars@gmail.com>
2024-02-04 10:03:49 +0000

    feat: add summary for outliers

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -153,7 +153,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "31dfe9a220fb6372"; }
+/******/ 		__webpack_require__.h = function() { return "9284568dac766241"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
