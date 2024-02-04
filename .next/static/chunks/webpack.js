8f0ce2ca8dcb81678d17a7314105c8bdc2c9821e
aleks <achudars@gmail.com>
2024-02-04 10:40:19 +0000

    feat: add summary for Range

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -153,7 +153,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "fd7e3a1e2f323936"; }
+/******/ 		__webpack_require__.h = function() { return "a08d6a246fd1b02c"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
