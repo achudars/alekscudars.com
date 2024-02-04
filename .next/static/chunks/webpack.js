5b9bd41969a8998eef50fb28dada1af945d5c60f
aleks <achudars@gmail.com>
2024-02-04 10:15:21 +0000

    feat: add summary for What the Dog Saw

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -153,7 +153,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "b1faf0486074d4a5"; }
+/******/ 		__webpack_require__.h = function() { return "a5f74c7c334948a2"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
