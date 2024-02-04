7f8505aed71f1fa33b9ca2fd8fedf3b41ea14430
aleks <achudars@gmail.com>
2024-02-04 10:42:40 +0000

    feat: add summary for the element

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -153,7 +153,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "a08d6a246fd1b02c"; }
+/******/ 		__webpack_require__.h = function() { return "8c8981a02cf50fd7"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
