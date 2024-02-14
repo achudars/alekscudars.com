11d79083fac9830e9f26d9400108f76ccd0b4d04
aleks <achudars@gmail.com>
2024-02-14 20:00:49 +0000

    feat: update assets

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -153,7 +153,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "cf0e51971977f853"; }
+/******/ 		__webpack_require__.h = function() { return "7c65244b4c77964c"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- .next/static/webpack/webpack.2bb4e9bd37888af3.hot-update.js
+++ .next/static/webpack/webpack.2bb4e9bd37888af3.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "9284568dac766241"; }
+/******/ 	__webpack_require__.h = function() { return "7c65244b4c77964c"; }
 /******/ }();
 /******/ 
 /******/ }
