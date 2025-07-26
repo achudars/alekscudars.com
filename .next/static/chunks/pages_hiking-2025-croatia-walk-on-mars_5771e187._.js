e2cf0420306b9125c3e5dad7f56ff50634123fc7
aleks <achudars@gmail.com>
2025-07-26 14:42:58 +0100

    chore: add trace files

---
+++ .next/static/chunks/pages_hiking-2025-croatia-walk-on-mars_5771e187._.js
@@ -0,0 +1,15 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_next_dist_compiled_next-devtools_index_82a36480.js",
+  "static/chunks/node_modules_next_dist_compiled_ca41998d._.js",
+  "static/chunks/node_modules_next_dist_shared_lib_fc27b909._.js",
+  "static/chunks/node_modules_next_dist_client_c0365516._.js",
+  "static/chunks/node_modules_next_dist_a3e9a08f._.js",
+  "static/chunks/node_modules_next_7c1ae270._.js",
+  "static/chunks/node_modules_react-dom_82bb97c6._.js",
+  "static/chunks/node_modules_a80c7711._.js",
+  "static/chunks/[root-of-the-server]__e414209f._.js"
+],
+    source: "entry"
+});
