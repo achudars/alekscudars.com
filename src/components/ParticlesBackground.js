efb45b791c6b1b700eda6b5bb1f8bbdbcf3a3365
aleks <achudars@gmail.com>
2026-05-23 21:55:29 +0100

    chore: migrate from npm to pnpm
    - Remove package-lock.json, add pnpm-lock.yaml
    - Add netlify.toml to configure pnpm build for Netlify
    - Add packageManager field to package.json
    - Fix ParticlesBackground to use ParticlesProvider API (tsparticles v4)

--- src/components/ParticlesBackground.js
+++ src/components/ParticlesBackground.js
@@ -1,21 +1,8 @@
-import { useEffect, useMemo } from "react";
-import Particles, { initParticlesEngine } from "@tsparticles/react";
+import { useMemo, useCallback } from "react";
+import Particles, { ParticlesProvider } from "@tsparticles/react";
 import { loadFull } from "tsparticles";
 
-const ParticlesBackground = () => {
-  // this should be run only once per application lifetime
-  useEffect(() => {
-    (async () => {
-      await initParticlesEngine(async (engine) => {
-        // you can initiate the tsParticles instance (engine) here, adding custom shapes or presets
-        // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
-        // starting from v2 you can add only the features you need reducing the bundle size
-        //await loadAll(engine);
-        await loadFull(engine);
-      });
-    })();
-  }, []);
-
+const ParticlesInner = () => {
   const options = useMemo(
     () => ({
       fullScreen: {
@@ -84,11 +71,23 @@ const ParticlesBackground = () => {
       },
       detectRetina: true,
     }),
-    []
+    [],
   );
 
   return (
     <Particles id="particles-box" className="particles" options={options} />
+  );
+};
+
+const ParticlesBackground = () => {
+  const init = useCallback(async (engine) => {
+    await loadFull(engine);
+  }, []);
+
+  return (
+    <ParticlesProvider init={init}>
+      <ParticlesInner />
+    </ParticlesProvider>
   );
 };
 
